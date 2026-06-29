b75273dfa09ed84a7c5a3ae75f298468530ad44b
kaziontor7 <kaziontor7@gmail.com>
2026-06-29 23:37:08 +0600

    optimized for deploy

--- src/lib/stripe.js
+++ src/lib/stripe.js
@@ -2,4 +2,4 @@ import 'server-only'
 
 import Stripe from 'stripe'
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || 'sk_test_dummy_key_to_prevent_build_errors_51')
